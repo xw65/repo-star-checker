e6f8208803bf1bc4edb9276e2e9ab2da222ec53a
Chenguang Liu <chenguang.liu@citrix.com>
2016-10-16 16:13:13 -0700

    use react js to refactor webpage

--- README.md
+++ README.md
@@ -7,22 +7,9 @@
 ![Alt](/sc.png "demo")
 
 ###Add your repo
-* Just add the full name of your repo into the following array
-* Please copy from the url of your repo instead of typing it
-~~~~
-$scope.repos = [
-  "lianliu/LianBlog",
-  "lianliu/SF-Muni",
-  "lianliu/lianliu.github.io",
-  "lianliu/mean_practice",
-  "lianliu/DEBS2016",
-  "S1ngS1ng/HammerSpoon",
-  "Zhou42/WeatherRadar"
-];
-~~~~
+* Use the input box to add your repo with format 'userName/repoName'
 
 ###Todo
-* extract repo list to a property file
 * use GitHub auth & star from UI
 * one click star all
 * display repo detail on UI
--- app.js
+++ app.js
@@ -1,114 +1,421 @@
-//references
-//https://developer.github.com/v3/activity/starring
-//https://developer.github.com/v3/#http-verbs
-
-var config = {
+// Constants
+const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCGRzE7appRUtxfOq8tM_wP4vfJrZGkgOQ",
   authDomain: "repostar-6a542.firebaseapp.com",
   databaseURL: "https://repostar-6a542.firebaseio.com",
   storageBucket: "repostar-6a542.appspot.com",
   messagingSenderId: "478402567163"
 };
-firebase.initializeApp(config);
+const URL_PREFIX = "https://github.com/";
 
-var app = angular.module('myApp', []);
+// Styles
+const alertStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  zIndex: 200
+};
+const titleStyle = {
+  marginTop: 20,
+  zIndex: 100
+};
+const titleStarStyle = {
+  marginRight: 10
+};
+const listStarStyle = {
+  marginRight: 4
+};
+const thumbnailStyle = {
+  marginBottom: 0
+};
+const textStyle = {
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden'
+};
 
-app.controller('MainCtrl', function ($scope, $http) {
+// Components
+class TitleBoxComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  //todo: property file, auth & star, star all, repo detail
+  render() {
+    return (
+      <div>
+        <div id="alert" className="alert alert-danger" style={alertStyle} hidden>
+          <strong>Error!</strong> {this.props.message}
+        </div>
+        <div className="jumbotron" style={titleStyle}>
+          <h2>
+            <a href="https://github.com/gjk0090/repo-star-checker">
+              <span className="glyphicon glyphicon-star" style={titleStarStyle} />
+            </a>
+            GitHub Stargazer
+          </h2>
+          <p>Have you starred these repos?</p>
+        </div>
+      </div>
+    );
+  }
+}
 
-  // Read repo list data from Firebase
-  var baseRef = firebase.database().ref('repo');
-  baseRef.once('value').then(function (snapshot) {
-    if (snapshot) {
-      var userDataMap = snapshot.val();
-      var repoList = [];
-      for (var userName in userDataMap) {
-        var userRepoList = userDataMap[userName];
-        for (var i = 0; i < userRepoList.length; i++) {
-          repoList.push(userName + "/" + userRepoList[i]);
-        }
-      }
+class AddBoxComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      defaultStyle: true,
+      repoUrl: ''
+    };
+    this._handleChange = this._handleChange.bind(this);
+    this._handleKeyPress = this._handleKeyPress.bind(this);
+    this._handleAction = this._handleAction.bind(this);
+  }
 
-      $scope.repos = repoList;
+  render() {
+    const buttonClassName = this.state.defaultStyle ? 'btn btn-default' : 'btn btn-primary';
+    return (
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="input-group">
+            <input type="text" className="form-control"
+                   value={this.state.repoUrl}
+                   placeholder="Enter your GitHub repo in userName/repoName format to add in the list"
+                   onKeyPress={this._handleKeyPress}
+                   onChange={this._handleChange} />
+            <span className="input-group-btn">
+              <button className={buttonClassName} type="button" onClick={this._handleAction}>Add!</button>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-      $scope.repos.sort();
+  _handleChange(e) {
+    this.setState({
+      defaultStyle: e.target.value === '',
+      repoUrl: e.target.value
+    });
+  }
 
-      $scope.urlPrefix = "https://github.com/";
+  _handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this._handleAction();
+    }
+  }
 
-      $scope.starredMap = {};
-
-      //extract user list
-      $scope.userList = [];
-      angular.forEach($scope.repos, function (repo, index) {
-        var user = repo.split("/")[0];
-        if ($scope.userList.indexOf(user) >= 0) {
-          return;
-        }
-        $scope.userList.push(user);
-      });
-
-      $('#progress-bar').fadeOut('fast', function () {
-        $('#repo-list').fadeIn();
-      });
-
-      //get all user image
-      $scope.imageMap = {};
-      angular.forEach($scope.userList, function (user, index) {
-
-        var url = "https://api.github.com/users/" + user;
-        $http({
-          method: "GET",
-          url: url
-        }).then(function mySucces(response) {
-          var image = response.data.avatar_url;
-
-          angular.forEach($scope.repos, function (repo, index2) {
-            var repo_user = repo.split("/")[0];
-            if (repo_user === user) {
-              $scope.imageMap[repo] = image;
-            }
+  _handleAction() {
+    if (this.state.repoUrl === '') {
+      return;
+    }
+    const _this = this;
+    const repoUrl = this.state.repoUrl;
+    const checkUrl = 'https://api.github.com/repos/' + repoUrl;
+    _this.setState({
+      defaultStyle: true,
+      repoUrl: ''
+    });
+    $.ajax({
+      url: checkUrl,
+      method: 'GET',
+      dataType: 'json',
+      cache: true,
+      success: function (data) {
+        $('#content').fadeOut('fast', function () {
+          $('#progress-bar').fadeIn();
+        });
+        const splits = repoUrl.split('/');
+        const baseRef = firebase.database().ref('repo');
+        const userRepoList = [];
+        userRepoList.push(splits[1]);
+        baseRef.child(splits[0]).set(userRepoList)
+          .then(function () {
+            _this.props.addAction();
+          })
+          .catch(function (err) {
+            console.log('Save to firebase failed: ' + err.toString());
+            const message = "Save data failed: " + err.toString();
+            _this.props.errorAction(message);
           });
-        }, function myError(response) {
+      }.bind(_this),
+      error: function (xhr, status, err) {
+        console.error(checkUrl, status, err.toString());
+        const message = "The repo " + repoUrl + " does not exist";
+        _this.props.errorAction(message);
+      }.bind(_this)
+    });
+  }
+}
 
+class InputBoxComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      defaultStyle: true,
+      userName: ''
+    };
+    this._handleChange = this._handleChange.bind(this);
+    this._handleKeyPress = this._handleKeyPress.bind(this);
+    this._handleAction = this._handleAction.bind(this);
+  }
+
+  render() {
+    const buttonClassName = this.state.defaultStyle ? 'btn btn-default' : 'btn btn-primary';
+    return (
+      <div className="row">
+        <br />
+        <div className="col-lg-12">
+          <div className="input-group">
+            <input type="text" className="form-control"
+                   value={this.state.userName}
+                   placeholder="Enter your GitHub username to check if you have starred these repos"
+                   onKeyPress={this._handleKeyPress}
+                   onChange={this._handleChange} />
+            <span className="input-group-btn">
+              <button className={buttonClassName} type="button" onClick={this._handleAction}>Go!</button>
+            </span>
+          </div>
+        </div>
+        <br />
+      </div>
+    );
+  }
+
+  _handleChange(e) {
+    this.setState({
+      defaultStyle: e.target.value === '',
+      userName: e.target.value
+    });
+  }
+
+  _handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this._handleAction();
+    }
+  }
+
+  _handleAction() {
+    const userName = this.state.userName;
+    this.setState({
+      defaultStyle: true,
+      userName: ''
+    });
+    if (userName && userName.length > 0) {
+      this.props.goAction(userName);
+    }
+  }
+}
+
+class ProgressBarComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="progress-bar">
+        <div className="progress progress-striped active">
+          <div className="progress-bar" role="progressbar"
+               aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"
+               style={{ width: '100%' }}>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class RepoListComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const _this = this;
+    const repoNodes = this.props.data.map(function (repo, index) {
+      const starClassName = _this.props.starredMap[repo.fullUrl] ? 'glyphicon glyphicon-star' : 'glyphicon glyphicon-star-empty';
+      return (
+        <li key={index} className="list-group-item">
+          <div className="row">
+            <div className="col-md-2 col-sm-2 col-xs-2">
+              <a className="thumbnail" style={thumbnailStyle}>
+                <img src={_this.props.imageMap[repo.userName]} />
+              </a>
+            </div>
+
+            <div className="col-md-10 col-sm-10 col-xs-10">
+              <h4 style={textStyle}>
+                {repo.fullUrl}
+              </h4>
+              <p style={textStyle}>{URL_PREFIX + repo.fullUrl}</p>
+              <div>
+                <span className="pull-right">
+                  <a>
+                    <span className={starClassName} style={listStarStyle} />
+                  </a>
+                  <a href={URL_PREFIX + repo.fullUrl}>
+                    <span className="glyphicon glyphicon-share" aria-hidden="true" />
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </li>
+      );
+    });
+    return (
+      <div id="content" hidden>
+        <AddBoxComponent addAction={this.props.addAction} errorAction={this.props.errorAction} />
+        <InputBoxComponent goAction={this.props.goAction} />
+        <br />
+        <ul id="repo-list" className="list-group">
+          {repoNodes}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class ContainerBoxComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    firebase.initializeApp(FIREBASE_CONFIG);
+    this.state = {
+      dataMap: {},
+      dataList: [],
+      imageMap: {},
+      starredMap: {},
+      error: false,
+      message: '',
+      timeout: -1
+    };
+    this._loadDataFromFirebase = this._loadDataFromFirebase.bind(this);
+    this._loadUserImages = this._loadUserImages.bind(this);
+    this._checkUserStarredList = this._checkUserStarredList.bind(this);
+    this._transformStarredList = this._transformStarredList.bind(this);
+    this._showAlertMessage = this._showAlertMessage.bind(this);
+  }
+
+  componentDidMount() {
+    this._loadDataFromFirebase();
+  }
+
+  _loadDataFromFirebase() {
+    const _this = this;
+    const baseRef = firebase.database().ref('repo');
+    baseRef.once('value').then(function (snapshot) {
+      if (snapshot) {
+        const userDataMap = snapshot.val();
+        _this._loadUserImages(userDataMap);
+        const repoList = [];
+        for (const userName in userDataMap) {
+          const userRepoList = userDataMap[userName];
+          for (var i = 0; i < userRepoList.length; i++) {
+            repoList.push({ userName: userName, fullUrl: userName + "/" + userRepoList[i] });
+          }
+        }
+        _this.setState({
+          dataMap: userDataMap,
+          dataList: repoList
         });
+        $('#progress-bar').fadeOut('fast', function () {
+          $('#content').fadeIn();
+        });
+      }
+    });
+  }
 
+  _loadUserImages(userDataMap) {
+    const _this = this;
+    const imageMap = this.state.imageMap;
+    for (const userName in userDataMap) {
+      const url = "https://api.github.com/users/" + userName;
+      $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'json',
+        cache: true,
+        success: function (data) {
+          imageMap[userName] = data.avatar_url;
+          _this.setState({
+            imageMap: imageMap
+          });
+        }.bind(_this),
+        error: function (xhr, status, err) {
+          console.error(url, status, err.toString());
+          const message = "Failed to load profile image for " + userName;
+          _this._showAlertMessage(message);
+        }.bind(_this)
       });
+    }
+  }
 
-      $scope.go = function (ev) {
-        if (!!ev && ev.keyCode !== 13) {
-          return;
-        }
-        if (!$scope.username) {
-          $scope.starredMap = {};
-          return;
-        }
-
-        var url = "https://api.github.com/users/" + $scope.username + "/starred?per_page=999";
-
-        $http({
-          method: "GET",
-          url: url
-        }).then(function mySucces(response) {
-          $scope.starredMap = $scope.transformStarredList(response.data);
-        }, function myError(response) {
-          $scope.starredMap = {};
-          $scope.errMsg = response.statusText;
-          alert("Error : " + $scope.errMsg);
+  _checkUserStarredList(userName) {
+    if (!userName) {
+      return;
+    }
+    const _this = this;
+    const url = "https://api.github.com/users/" + userName + "/starred?per_page=999";
+    $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'json',
+      cache: true,
+      success: function (data) {
+        var newStarredMap = this._transformStarredList(data);
+        _this.setState({
+          starredMap: newStarredMap
         });
-      };
+      }.bind(_this),
+      error: function (xhr, status, err) {
+        console.error(url, status, err.toString());
+        const message = "Failed to find user: " + userName;
+        _this._showAlertMessage(message);
+      }.bind(_this)
+    });
+  }
 
-
-      $scope.transformStarredList = function (starredList) {
-        var outputMap = {};
-        if (!starredList) {
-          return outputMap;
-        }
-        angular.forEach(starredList, function (repo, index) {
-          outputMap[repo.full_name] = true;
-        });
-        return outputMap;
+  _transformStarredList(starredList) {
+    const map = {};
+    if (starredList) {
+      for (var i = 0; i < starredList.length; i++) {
+        map[starredList[i].full_name] = true;
       }
     }
-  });
-});
+    return map;
+  }
+
+  _showAlertMessage(message) {
+    const prevTimeout = this.state.timeout;
+    if (prevTimeout != -1) {
+      clearTimeout(prevTimeout);
+    }
+    this.setState({
+      message: message
+    });
+    $('#alert').show();
+    const timeout = setTimeout(function () {
+      $('#alert').hide();
+    }, 3000);
+    this.setState({
+      timeout: timeout
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <TitleBoxComponent error={this.state.error} message={this.state.message} />
+        <ProgressBarComponent />
+        <RepoListComponent data={this.state.dataList} imageMap={this.state.imageMap}
+                           starredMap={this.state.starredMap} goAction={this._checkUserStarredList}
+                           addAction={this._loadDataFromFirebase} errorAction={this._showAlertMessage} />
+      </div>
+    );
+  }
+}
+
+// Render
+ReactDOM.render(<ContainerBoxComponent />, document.getElementById('container'));
