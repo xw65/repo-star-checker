89e501311158daa8ba1f19d88abad3d8c0cc63ac
xw65 <xiaohang.wang.th@gmail.com>
2016-12-04 16:30:07 -0800

    remove babel compile

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-import express from 'express'
-import hogan from 'hogan-express'
+var React =  require('react');
+var ReactDOMServer = require('react-dom/server');
+var express = require('express');
+var hogan = require('hogan-express');
 
 // Express
-const app = express()
+const app = express();
 app.engine('html', hogan);
 app.set('views', __dirname + '/views');
 app.use('/', express.static(__dirname));
 app.set('port', 8080);
 
-app.get('*',(req, res) => {
+app.get('*', (req, res) => {
     res.status(200).render('index.html');
 });
 
 app.listen(app.get('port'));
 
-console.info('==> ✅  Server is listening in ' + process.env.NODE_ENV + ' mode')
-console.info('==> 🌎  Go to http://localhost:%s', app.get('port'))
+console.info('==> ✅  Server is listening in ' + process.env.NODE_ENV + ' mode');
+console.info('==> 🌎  Go to http://localhost:%s', app.get('port'));
